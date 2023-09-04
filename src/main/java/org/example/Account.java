8f5dd47d513ec69ef55db6a4cc2c69a48ced09c9
valeriearing <the.v.aring@gmail.com>
2023-09-04 14:51:45 +0200

    Init

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class Account {
+    String accountNR;
+    BigDecimal balance;
+    Client client;
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+/*
+Erstellt eine Record 'Client' mit den Eigenschaften Vorname, Nachname und Kundennummer (wähle geeignete englische Feldnamen).
+
+Erstellt eine Klasse 'Account' (kein Record, soll vorerst veränderlich sein) mit den Eigenschaften Kontonummer (String),
+Kontostand (BigDecimal) und dem zugehörigen Kunden.
+
+Implementiert eine Methode, um Geld auf das Konto einzuzahlen.
+
+Implementiert eine Methode, um Geld vom Konto abzuheben.
+
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 4.00;
+        double b = 0.50;
+
+        System.out.println(a/b);
+    }
+}
